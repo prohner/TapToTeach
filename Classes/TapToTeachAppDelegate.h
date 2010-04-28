053470713bf1a3e2747ef972d172b6640379c69f
Preston Rohner <prestonrohner@me.com>
2010-04-27 20:49:04 -0700

    First commit

---
+++ Classes/TapToTeachAppDelegate.h
@@ -0,0 +1,30 @@
+//
+//  TapToTeachAppDelegate.h
+//  TapToTeach
+//
+//  Created by Preston Rohner on 4/27/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface TapToTeachAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
